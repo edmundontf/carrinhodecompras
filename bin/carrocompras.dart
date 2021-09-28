f05394a7e5963123667d0b3e4a708b028460f99a
edmundontf@gmail.com <edmundofaria@Edmundos-MacBook-Pro.local>
2021-09-28 10:33:35 -0300

    Modularização do  programa

--- bin/carrocompras.dart
+++ bin/carrocompras.dart
@@ -5,22 +5,37 @@ main() {
   bool condicao = true;
 
   while (condicao) {
-    print("Digite um produto para adicionar ao carrinho");
+    print("=== Digite um produto para adicionar ou digite remover | sair ===");
     String texto = stdin.readLineSync() ?? "";
     if (texto == 'sair') {
-      print('tem certeza q deseja finalizar sua compra?');
-      String op = stdin.readLineSync() ?? "";
-      if (op == 'sim') {
-        condicao = false;
-        print("Você saiu!");
-      }
+      sair(condicao, texto);
+      condicao = false;
+    } else if (texto == 'imprimir') {
+      imprimir(produtos, texto);
     } else if (texto == 'remover') {
-      print("Qual o produto você deseja remover?");
-      print("carrinho: $produtos");
-      produtos.remove(texto = stdin.readLineSync() ?? "");
+      remover(produtos, texto);
     } else {
       produtos.add(texto);
     }
-    print("carrinho: $produtos");
+  }
+}
+
+imprimir(List<String> produtos, texto) {
+  for (var i = 0; i < produtos.length; i++) {
+    print(" Item: $i -  ${produtos[i]}");
+  }
+}
+
+remover(List<String> produtos, String texto) {
+  print("Qual o produto você deseja remover?");
+  print("carrinho: $produtos");
+  produtos.remove(texto = stdin.readLineSync() ?? "");
+}
+
+sair(bool condicao, String texto) {
+  print('tem certeza q deseja finalizar sua compra?');
+  String texto = stdin.readLineSync() ?? "";
+  if (texto == 'sim') {
+    print("Você saiu!");
   }
 }
